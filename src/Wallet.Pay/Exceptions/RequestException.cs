d3d0cae90e9c679728d0f00b237bf117cfce3614
Vladislav Titov <capt.Hoffmann@gmail.com>
2024-06-11 12:47:11 +0300

    Wallet pay client interface was implemented #3

---
+++ src/Wallet.Pay/Exceptions/RequestException.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+
+namespace Wallet.Pay.Exceptions;
+
+public class RequestException : Exception
+{
+    public HttpStatusCode HttpStatusCode { get; }
+
+    public RequestException(string message) 
+        : base(message)
+    { }
+
+    public RequestException(string message, Exception exception) 
+        : base(message, exception)
+    { }
+
+    public RequestException(string message, HttpStatusCode httpStatusCode) 
+        : base(message)
+    {
+        HttpStatusCode = httpStatusCode;
+    }
+
+    public RequestException(string message, HttpStatusCode httpStatusCode, Exception innerException) 
+        : base(message, innerException)
+    {
+        HttpStatusCode = httpStatusCode;
+    }
+}
---
+++ src/Wallet.Pay/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Wallet.Pay.Extensions;
+
+internal static class HttpClientExtensions
+{
+    internal static HttpClient AddRequestHeader(this HttpClient httpClient, string headerName, string value)
+    {
+        httpClient.DefaultRequestHeaders.Add(headerName, value);
+        return httpClient;
+    }
+}
