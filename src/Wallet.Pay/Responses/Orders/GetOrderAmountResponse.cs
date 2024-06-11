6fe3c7c28b246b2a6fb54312a1a9b3e549e40581
Vladislav Titov <capt.Hoffmann@gmail.com>
2024-06-11 15:13:44 +0300

    Order responses were added #4

---
+++ src/Wallet.Pay/Responses/Orders/GetOrderAmountResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Wallet.Pay.Responses.Orders;
+
+#nullable disable
+public class GetOrderAmountResponse
+{
+    public string TotalAmount { get; set; }
+}
---
+++ src/Wallet.Pay/Responses/Orders/GetOrderListResponse.cs
@@ -0,0 +1,26 @@
+﻿using Wallet.Pay.Models;
+
+namespace Wallet.Pay.Responses.Orders;
+
+#nullable disable
+public class GetOrderListResponse
+{
+    public IEnumerable<Item> Items { get; set; }
+}
+
+public class Item : Order
+{
+    public string ExternalId { get; set; }
+    public string CustomerTelegramUserId { get; set; }
+    public DateTimeOffset PaymentDateTime { get; set; }
+    public SelectedPaymentOption SelectedPaymentOption { get; set; }
+}
+
+public class SelectedPaymentOption 
+{ 
+    public Amount Amount { get; set; }
+    public Amount AmountFee { get; set; }
+    public Amount AmountNet {  get; set; }
+    public string ExchangeRate { get; set; }
+}
+
