2b3106aea49834dd1a339d9b967bdda70f059fa3
Vladislav Titov <capt.Hoffmann@gmail.com>
2024-06-11 15:12:26 +0300

    Order model was added #4

---
+++ src/Wallet.Pay/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using Wallet.Pay.Enums;
+
+namespace Wallet.Pay.Models;
+
+#nullable disable
+public class Order
+{
+    public string Id { get; set; }
+    public OrderStatus Status { get; set; }
+    public Amount Amount { get; set; }
+    public Currency AutoConversionCurrency { get; set; }
+    public DateTimeOffset CreatedDateTime { get; set; }
+    public DateTimeOffset ExpirationDateTime { get; set; }
+}
