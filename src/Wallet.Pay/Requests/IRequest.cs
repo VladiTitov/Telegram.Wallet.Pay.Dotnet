db57f115e2d2772de17ef9e79d370b9c92dc323a
Vladislav Titov <capt.Hoffmann@gmail.com>
2024-06-11 12:40:53 +0300

    Base request model was added #3

---
+++ src/Wallet.Pay/Requests/IRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Wallet.Pay.Requests;
+
+public interface IRequest<TResponse>
+{
+    HttpMethod Method { get; }
+    string UriPath { get; }
+    HttpContent ToHttpContent();
+}
---
+++ src/Wallet.Pay/Requests/RequestBase.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace Wallet.Pay.Requests;
+
+#nullable disable
+public class RequestBase<TResponse>(string uriPath, HttpMethod method) : IRequest<TResponse>
+{
+    [JsonIgnore]
+    public HttpMethod Method { get; } = method;
+
+    [JsonIgnore]
+    public string UriPath { get; } = uriPath;
+
+    public RequestBase(string uriPath) 
+        : this(uriPath, HttpMethod.Post)
+    { }
+
+    public HttpContent ToHttpContent()
+    {
+        var content = JsonConvert.SerializeObject(
+            value: this,
+            new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            });
+
+        return new StringContent(
+            content: content,
+            encoding: Encoding.UTF8,
+            mediaType: "application/json");
+    }
+}
