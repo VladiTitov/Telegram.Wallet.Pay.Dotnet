8ce5f5ef36f4350ec96cf03f31c9bd2d12668afc
Vladislav Titov <capt.Hoffmann@gmail.com>
2024-06-11 12:41:16 +0300

    Base response model was added #3

---
+++ src/Wallet.Pay/Responses/IResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Wallet.Pay.Responses;
+
+internal interface IResponse<TResponse>
+{
+    public string Status { get; set; }
+    public string Message { get; set; }
+    public TResponse Data { get; set; }
+}
---
+++ src/Wallet.Pay/Responses/ResponseBase.cs
@@ -0,0 +1,9 @@
+﻿namespace Wallet.Pay.Responses;
+
+#nullable disable
+internal class ResponseBase<TResponse> : IResponse<TResponse>
+{
+    public string Status { get; set; }
+    public string Message { get; set; }
+    public TResponse Data { get; set; }
+}
