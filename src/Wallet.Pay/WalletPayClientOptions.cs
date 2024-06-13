bfd213766d333689c1daf67f1ea6e6e1ba1ea8b4
Vladislav Titov <capt.Hoffmann@gmail.com>
2024-06-13 09:39:02 +0300

    Summaries were added in wallet pay client #13

--- src/Wallet.Pay/WalletPayClientOptions.cs
+++ src/Wallet.Pay/WalletPayClientOptions.cs
@@ -1,8 +1,16 @@
 ﻿namespace Wallet.Pay;
 
+/// <summary>
+/// This class is used to provide configuration for <see cref="WalletPayClient"/>
+/// </summary>
+/// <param name="token">Wallet Pay API token</param>
 #nullable disable
 public class WalletPayClientOptions(string token)
 {
     internal readonly string BaseWalletPayUri = "https://pay.wallet.tg";
+
+    /// <summary>
+    /// Wallet Pay API token
+    /// </summary>
     public string Token { get; } = token;
 }
